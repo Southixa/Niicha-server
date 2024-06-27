71be0b374ab302d94b57ebeb8becb6a6c075db15
[southixa phlavong] <[southixa.pele10@gmail.com]>
2024-06-27 17:05:34 +0700

    fix: change db to cloud cluster

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,23 @@
 import mysql from "mysql";
 
+// const con = mysql.createConnection({
+//   host: "127.0.0.1",
+//   port: "3308",
+//   user: "root",
+//   password: "",
+//   database: "niichadb",
+//   timezone: 'z',
+//   connectionLimit: 50,
+//   queueLimit: 0,
+//   waitForConnection: true
+// });
+
 const con = mysql.createConnection({
-  host: "127.0.0.1",
-  port: "3308",
-  user: "root",
-  password: "",
-  database: "niichadb",
+  host: "mysql-176882-0.cloudclusters.net",
+  port: "19673",
+  user: "admin",
+  password: "QKKXdMfb",
+  database: "NiichaDB",
   timezone: 'z',
   connectionLimit: 50,
   queueLimit: 0,
