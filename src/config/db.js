4017c06da5b6d2ef8ee64857e6b28ea7a3993847
[southixa phlavong] <[southixa.pele10@gmail.com]>
2024-06-27 13:54:35 +0700

    fix: order get all join table

--- src/config/db.js
+++ src/config/db.js
@@ -15,36 +15,40 @@ const con = mysql.createConnection({
   user: "admin",
   password: "GkrsCq81",
   database: "NiichaDB",
-  timezone: 'z'
+  timezone: 'z',
 });
 
 // Function to handle connection
 const connectToDatabase = () => {
   con.connect((err) => {
-    if (err) {
-      if (err.code === 'ECONNRESET') {
-        console.error('Connection was reset by the server:', err);
-        // Optionally attempt to reconnect
-      } else {
-        console.error('Error connecting to the database:', err);
-      }
-    } else {
-      console.log('Connected to the Database!');
-    }
+    console.log("con.connect =>");
+    console.log(err);
+    // if (err) {
+    //   if (err.code === 'ECONNRESET') {
+    //     console.error('Connection was reset by the server:', err);
+    //     // Optionally attempt to reconnect
+    //   } else {
+    //     console.error('Error connecting to the database:', err);
+    //   }
+    // } else {
+    //   console.log('Connected to the Database!');
+    // }
   });
 
   // Handle connection errors after initial connection
   con.on('error', (err) => {
-    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-      console.error('Database connection was closed.');
-      connectToDatabase(); // Reconnect on connection loss
-    } else if (err.code === 'ECONNRESET') {
-      console.error('Connection was reset by the server:', err);
-      connectToDatabase(); // Optionally attempt to reconnect
-    } else {
-      console.error('Database error:', err);
-      throw err;
-    }
+    console.log("con.on =>");
+    console.log(err);
+    // if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+    //   console.error('Database connection was closed.');
+    //   connectToDatabase(); // Reconnect on connection loss
+    // } else if (err.code === 'ECONNRESET') {
+    //   console.error('Connection was reset by the server:', err);
+    //   connectToDatabase(); // Optionally attempt to reconnect
+    // } else {
+    //   console.error('Database error:', err);
+    //   throw err;
+    // }
   });
 };
 
