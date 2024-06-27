68fa061dc76a4131dda4d2c66107a3797071ce38
[southixa phlavong] <[southixa.pele10@gmail.com]>
2024-06-27 21:06:02 +0700

    fix: createPool on connection sql db

--- src/config/db.js
+++ src/config/db.js
@@ -1,59 +1,16 @@
 import mysql from "mysql";
 
-// // const con = mysql.createConnection({
-// //   host: "127.0.0.1",
-// //   port: "3308",
-// //   user: "root",
-// //   password: "",
-// //   database: "niichadb",
-// //   timezone: 'z',
-// //   connectionLimit: 50,
-// //   queueLimit: 0,
-// //   waitForConnection: true
-// // });
-
-const con = mysql.createConnection({
+const con = mysql.createPool({
+  connectionLimit: 50,  // Adjust as needed
+  waitForConnections: true,
+  queueLimit: 0,
   host: "mysql-176882-0.cloudclusters.net",
   port: "19673",
   user: "admin",
   password: "QKKXdMfb",
   database: "NiichaDB",
-  timezone: 'z',
-  connectionLimit: 50,
-  queueLimit: 0,
-  waitForConnection: true
+  timezone: 'z'
 });
 
-
-// Function to handle connection
-const connectToDatabase = () => {
-  con.connect((err) => {
-    if (err) {
-      if (err.code === 'ECONNRESET') {
-        console.error('Connection was reset by the server:', err);
-        // Optionally attempt to reconnect
-      } else {
-        console.error('Error connecting to the database:', err);
-      }
-    } else {
-      console.log('Connected to the Database!');
-    }
-  });
-  // Handle connection errors after initial connection
-  con.on('error', (err) => {
-    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-      console.error('Database connection was closed.');
-      connectToDatabase(); // Reconnect on connection loss
-    } else if (err.code === 'ECONNRESET') {
-      console.error('Connection was reset by the server:', err);
-      connectToDatabase(); // Optionally attempt to reconnect
-    } else {
-      console.error('Database error:', err);
-      throw err;
-    }
-  });
-};
-// Connect to the database
-connectToDatabase();
-
 export default con;
+
